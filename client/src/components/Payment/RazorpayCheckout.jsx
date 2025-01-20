f09710abbed14bfe8e5736326fefe2d5ff1a0726
PRASHANT KUMAR DAS <131296871+theprashantkumardas@users.noreply.github.com>
2025-01-21 01:55:23 +0530

    Update RazorpayCheckout.jsx

--- client/src/components/Payment/RazorpayCheckout.jsx
+++ client/src/components/Payment/RazorpayCheckout.jsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import React, { useState} from 'react';
 const RazorpayCheckout = ({amount, onPaymentSuccess}) => {
     const [orderId, setOrderId] = useState('');
     const [loading, setLoading] = useState(false)
