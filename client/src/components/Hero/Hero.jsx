4459c5c8c1b7c026cab4e3fdd2fa7719e470841c
PRASHANT KUMAR DAS <131296871+theprashantkumardas@users.noreply.github.com>
2025-01-21 01:54:18 +0530

    Update Hero.jsx

--- client/src/components/Hero/Hero.jsx
+++ client/src/components/Hero/Hero.jsx
@@ -55,7 +55,7 @@ const Hero = () => {
 
 
                 <div className="md:w-1/2 ">
-                    <img src="https://images.unsplash.com/photo-1642543348745-03b1219733d9?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="Hero Image" className="w-full rounded-3xl object-cover h-auto md:h-full" />
+                    <img src="https://images.unsplash.com/photo-1642543348745-03b1219733d9?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="Hero" className="w-full rounded-3xl object-cover h-auto md:h-full" />
                 </div>
 
 
